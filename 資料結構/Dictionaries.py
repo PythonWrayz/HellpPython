a40bed8c5f70c6e7f00e6fc6c1f4be325b47460d
WRayz <ruizhouwu@gmail.com>
2019-01-07 00:16:45 +0800

    Dictionaries demo

---
+++ 資料結構/Dictionaries.py
@@ -0,0 +1,27 @@
+# dict
+point = {"x": 1, "y": 2}
+# 建議寫法
+point = dict(x=1, y=2, z=3)
+print(point["x"])
+# 修改值
+point["x"] = 10
+print(point["x"])
+# 如果 key 不存在 dict，會報錯，所以
+if "z" in point:
+    print(point["z"])
+# 更好的取值方法
+print(point.get("x"))
+print(point.get("a"))  # None
+print(point.get("a", 0))  # 可像 SQL 的 ISNULL 設定預設值
+# 刪除
+del point["x"]
+print(point)
+# for loop 操作
+for key in point:
+    print(key, point[key])
+# 用 items 方法
+for item in point.items():
+    print(item)
+# items() + tuple 特性
+for key, value in point.items():
+    print(key, value)
