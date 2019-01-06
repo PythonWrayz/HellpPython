6a07a651ccf5f5355c1c509a1fe25da0dd36d178
WRayz <ruizhouwu@gmail.com>
2019-01-07 00:53:42 +0800

    Generator object 說明

--- 資料結構/Tuples.py
+++ 資料結構/Tuples.py
@@ -1,5 +1,6 @@
 # Tuple 特性是 readonly list：無法被 modify
 # 使用 Tuple 的時機，我認為跟 C# 使用 readonly 關鍵字的理由相同，從外部注入且不允許被修改的 list
+from sys import getsizeof
 tuple1 = (1, 2)
 tuple2 = 1, 2
 print(type(tuple2))
@@ -22,3 +23,14 @@ print(tuple7[0:2])
 x, y, a, b = tuple7
 if 10 in tuple7:
     print("exists")
+
+# 使用 Comprehensions 建立
+values = (x for x in range(10))
+# 得到 generator object
+print(values)
+# 可跑 loop
+for value in values:
+    print(value)
+
+# generator object 只佔用固定記憶體，不管被 generate 的資料有多大
+print("gen", getsizeof(values))
